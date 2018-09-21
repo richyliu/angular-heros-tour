2486cf1cc817b2eb9265381dc11795ba85ab1560
richyliu <richy.liu.2002@gmail.com>
2018-09-21 14:34:20 -0700

    migrate to angular 6

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 import {HEROES} from './mock-heroes';
 import {Hero} from './hero';
@@ -10,8 +11,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
 }
